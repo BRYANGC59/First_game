69f14090391c71a4e601067b86105f26612bf353
kenner <kenner.streamer@gmail.com>
2025-04-07 21:22:23 -0500

    CREACION DE COLICION

--- Game/elementos.py
+++ Game/elementos.py
@@ -14,9 +14,9 @@ class Enemigo:
         self.y = y
         self.gemas = random.randint(1,3)
 
-        arbol = os.path.join("recursos", "imagenes", "personajes", "echisera.png")
-        self.image = pygame.image.load(arbol).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (constantes.ARBOL, constantes.ARBOL))
+        Enemigo = os.path.join("recursos", "imagenes", "personajes", "echisera.png")
+        self.image = pygame.image.load(Enemigo).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (constantes.ENEMIGO, constantes.ENEMIGO))
         self.size = self.image.get_width()
 
     def draw(self, ventana):
@@ -30,7 +30,7 @@ class Roca:
 
         roca_paht = os.path.join("recursos", "imagenes", "personajes", "roca.png")
         self.image = pygame.image.load(roca_paht).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (constantes.ARBOL, constantes.ARBOL))
+        self.image = pygame.transform.scale(self.image, (constantes.ENEMIGO, constantes.ENEMIGO))
         self.size = self.image.get_width()
 
     def draw(self, ventana):
