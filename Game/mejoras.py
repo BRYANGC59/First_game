380735f99fe2f7cf6132755fb40cc109c50a4269
Bryan <bryangarciacastano@gmail.com>
2025-05-29 00:26:09 -0500

    funcion obtener_mejora_aleatoria

--- Game/mejoras.py
+++ Game/mejoras.py
@@ -2,3 +2,15 @@ class Mejora:
     def __init__(self, tipo, valor):
         self.tipo = tipo
         self.valor = valor
+
+    def obtener_mejora_aleatoria(self):
+        import random
+        opciones = [
+            Mejora("arma", "Espada de fuego"),
+            Mejora("arma", "Hacha pesada"),
+            Mejora("armadura", "Armadura de hierro"),
+            Mejora("armadura", "Armadura de sombras"),
+            Mejora("habilidad", "Velocidad extra"),
+            Mejora("habilidad", "Regeneración de vida")
+        ]
+        return random.choice(opciones)
