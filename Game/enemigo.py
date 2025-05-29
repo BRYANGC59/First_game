cafa790e3c645b5b5d71ee67bbe686c2bede6d46
Bryan <bryangarciacastano@gmail.com>
2025-05-29 00:15:13 -0500

    clase Enemigo_de_fuego

--- Game/enemigo.py
+++ Game/enemigo.py
@@ -17,6 +17,8 @@ class Enemigo:
     def esta_vivo(self):
         return self.vida > 0
 
-
+class Enemigo_de_fuego(Enemigo):
+    def __init__(self):
+        super().__init__("Demonio de Fuego", random.randint(30, 50), random.randint(5, 15))
 
 
