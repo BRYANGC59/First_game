ddaac5aeaaaebefd2a597ce1a53c1effd6c9ec95
Bryan <bryangarciacastano@gmail.com>
2025-05-29 00:09:19 -0500

    metodo esta_vivo y reiniciar

--- Game/jugador.py
+++ Game/jugador.py
@@ -30,33 +30,14 @@ class Jugador:
         elif mejora.tipo == "habilidad":
             self.inventario.append(mejora.valor)
 
+    def esta_vivo(self):
+        return self.vida > 0
 
-    def comprobar_colision(self, x, y, objeto):
-        return (x < objeto.x + objeto.size and x + self.size > objeto.x and y < objeto.y + objeto.size and
-                y + self.size > objeto.y)
+    def reiniciar(self):
+        self.vida = 100
+        self.arma = "Espada de madera"
+        self.armadura = "Armadura de cuero"
+        self.inventario = []
+        self.estrellas = 0
 
-    def esta_cerca(self, obj):
-        return (abs(self.x - obj.x) <= max(self.size, obj.size +5 and
-                abs(self.y - obj.y) <= max(self.size, obj.size)+5))
-
-
-    def interactuar(self, mundo):
-        for enemigo in mundo.enemigo:
-            if self.esta_cerca(enemigo):
-                if enemigo.soltar_gemas():
-                    self.inventario['GEMAS'] += 1
-                    if enemigo.gemas == 0:
-                        mundo.enemigo.remove(enemigo)
-                return
-
-    def draw_inventario(self, ventana):
-        backgrounds = pygame.Surface((constantes.ANCHO, constantes.ALTO), pygame.SRCALPHA)
-        backgrounds.fill((0,0,0,128))
-        ventana.blit(backgrounds, (0,0))
-
-        font = pygame.font.Font(None, 36)
-        titulo = font.render('Inventario', True, constantes.WHITE)
-        ventana.blit(titulo, (constantes.ANCHO // 2 - titulo.get_width() // 2, 20))
-
-        item_font = pygame.
 
